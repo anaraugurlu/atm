c1de9fd3eda32ba641d732cf172e2fb5b816309f
Ugur_ml97 <Ugur_ml97@itstep.edu.az>
2021-12-11 14:38:19 +0400

    nnew

--- atm/View/MainWindow.xaml.cs
+++ atm/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using atm.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace atm
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainViewModel(this);
         }
     }
 }
--- atm/ViewModels/MainViewModel.cs
+++ atm/ViewModels/MainViewModel.cs
@@ -6,11 +6,81 @@ using System.Threading.Tasks;
 
 namespace atm.ViewModels
 {
-    class MainViewModel : BaseViewModel
+    public class MainViewModel : BaseViewModel
     {
-        public MainViewModel()
+        //public RelayCommand SelectBookCommand { get; set; }
+        //public RelayCommand AddCommand { get; set; }
+        //public RelayCommand UpdateCommand { get; set; }
+        //public RelayCommand ResetCommand { get; set; }
+        //public RelayCommand DeleteOrderCommand { get; set; }
+        //public MainWindow main { get; set; }
+        //public RelayCommand DeleteBookCommand { get; set; }
+        public MainViewModel(MainWindow mainWindow)
         {
+            //SelectedBook = new Book();
+            //AllBooks = App.DB.BookRepository.GetAllData();
+            //DeleteBookCommand = new RelayCommand((sender) =>
+            //{
+            //    App.DB.BookRepository.DeleteData(SelectedBook.Id);
+            //    AllBooks = App.DB.BookRepository.GetAllData();
+            //    MessageBox.Show("Delete Book Succesfully");
 
-        }
+            //}, (pred) =>
+            //{
+            //    if (SelectedBook != null && SelectedBook.Id != 0)
+            //    {
+            //        return true;
+            //    }
+            //    return false;
+            //});
+            //ResetCommand = new RelayCommand((sender) =>
+            //{
+            //    SelectedBook = new Book();
+            //});
+            //SelectBookCommand = new RelayCommand((sender) =>
+            //{
+            //    if (SelectedBook != null)
+            //    {
+            //        var book = App.DB.BookRepository.GetData(SelectedBook.Id);
+            //        SelectedBook = book;
+            //    }
+            //});
+            //AddCommand = new RelayCommand((sender) =>
+            //{
+
+            //    var item = App.DB.BookRepository.GetData(SelectedBook.Id);
+
+            //    if (item.Id == 0)
+            //    {
+
+            //        App.DB.BookRepository.AddData(SelectedBook);
+            //        AllBooks = App.DB.BookRepository.GetAllData();
+        //            MessageBox.Show("Add Successfully");
+        //        }
+        //        else
+        //        {
+        //            MessageBox.Show("This customer is already exists");
+        //        }
+        //        SelectedBook = new Book();
+        //    });
+        //    UpdateCommand = new RelayCommand((sender) =>
+        //    {
+        //        App.DB.BookRepository.UpdateData(SelectedBook);
+        //        MessageBox.Show("Update Successfully");
+        //        SelectedBook = new Book();
+        //    });
+        //}
+        //private List<Book> allBooks;
+        //public List<Book> AllBooks
+        //{
+        //    get { return allBooks; }
+        //    set { allBooks = value; OnPropertyChanged(); }
+        //}
+        //private Book selectedBook;
+        //public Book SelectedBook
+        //{
+        //    get { return selectedBook; }
+        //    set { selectedBook = value; OnPropertyChanged(); }
+        //}
     }
 }
