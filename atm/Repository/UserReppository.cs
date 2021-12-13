e1462a89a20359a6bdc436f7d46c25d2fe0561d1
Ugur_ml97 <Ugur_ml97@itstep.edu.az>
2021-12-13 23:40:40 +0300

    new

---
+++ atm/Repository/UserReppository.cs
@@ -0,0 +1,37 @@
+﻿using atm.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atm.Repository
+{
+  public  class UserReppository
+    {
+        public List<User> GetAll()
+        {
+            return new List<User>
+{
+new User
+{
+Number =2222,
+Fullname="User1",
+Balance=2000
+},
+new User
+{
+Number=7777,
+Fullname="User2",
+Balance=200
+},
+new User
+{
+Number=3333,
+Fullname="User3",
+Balance=20
+}
+};
+        }
+    }
+}
--- atm/ViewModels/MainViewModel.cs
+++ atm/ViewModels/MainViewModel.cs
@@ -1,42 +1,74 @@
 ﻿using atm.Commands;
+using at36666666m.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using atm.Repository;
+using System.Windows.Threading;
+using System.Threading;
 
 namespace atm.ViewModels
 {
- \
+ 
     public class MainViewModel : BaseViewModel
     {
+
         public RelayCommand InsertButton { get; set; }
         public RelayCommand LoadDataButton { get; set; }
         public RelayCommand TransferMoneyButton { get; set; }
         public RelayCommand NameLbl { get; set; }
         public RelayCommand BalanceLbl { get; set; }
         public MainWindow main { get; set; }
+        public static object obj = new object();
+
         public RelayCommand PriceLbl { get; set; }
-        public MainViewModel(MainWindow mainWindow) { 
-        TransferMoneyButton = new RelayCommand((sender) =>
+        DispatcherTimer dispatcher = new DispatcherTimer();
+
+        public UserReppository  repository { get; set; }
+        public User User { get; set; }
+
+        public MainViewModel(MainWindow mainWindow) {
+
+            Users = new List<User>();
+            repository = new UserReppository();
+            Users = repository.GetAll();
+            TransferMoneyButton = new RelayCommand((sender) =>
             {
-        
-            
+
+                lock (obj)
+                {
+                    dispatcher.Start();
+                    if (decimal.Parse(User.Balance.ToString()) >= decimal.Parse(main.transfer .Text))
+                    {
+                        Thread.Sleep(5000);
+                        User.Balance = (decimal.Parse(User.Balance.ToString()) - decimal.Parse(main.transfer .Text .ToString ()).ToString ();
+                        main.balance.Content = User.Balance;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Transfer Declined");
+                    }
+                }
             });
              InsertButton = new RelayCommand((sender) =>
              {
-                 Users.Add ()
-        });
+                 main.insertlbl  .Visibility = Visibility.Visible;
+             });
             LoadDataButton = new RelayCommand((sender) =>
             {
                 foreach (var item in Users)
                 {
-                    if(item.Number==main.insertlbl .to)
+                    if (item.Number .ToString () == main.insertlbl .Text)
                     {
-
+                        main.name .Content = item.Fullname ;
+                        main.balance.Content = item.Balance;
+                        User = item;
                     }
                 }
-        });
+            });
         }
 
         private List<User> allUsers;
